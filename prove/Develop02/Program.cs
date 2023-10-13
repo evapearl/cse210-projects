4030445d4d3402d232db7acf9d7d70a70d290300
evapearl <122605689+evapearl@users.noreply.github.com>
2023-10-13 23:05:55 +0100

    week4

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Entry choice1 = new Choice();
+        choice1._idNumber = 1;
+        choice1._journalList = "Write" ;
+
+        
+        Entry choice2 = new Choice();
+        choice2._idNumber = 2;
+        choice2._journalList = "Display";
+
+
+        Entry choice3 = new Choice();
+        choice3._idNumber = 3;
+        choice3._journalList = "Load";
+
+
+        Entry choice4 = new Choice();
+        choice4._idNumber = 4;
+        choice4._journalList = "Save" ;
+
+
+        Entry choice5 = new Choice();
+        choice5._idNumber = 5;
+        choice5._journalList = "Quit" ;
+
+        Menu menulist = new Menu();
+        menulist.write().Add(choice1);
+        menulist.write().Add(prompt);
+
+        menulist.Display().Add(choice2);
+
+        menulist.Load().Add(choice3);
+
+        menulist.Save().Add(choice4);
+
+        menulist.Quit().Add(choice5);
+
+
+        PromptGenerator prompt = new Prompt();
+        prompt._promptList = "userprompt";
+
+        menulist.Display();
     }
 }
