3f7f0cf88acd461ac3d01e7179fb442ee7454f2f
evapearl <122605689+evapearl@users.noreply.github.com>
2023-10-27 02:31:40 +0100

    week 7 learning

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Rose Gary", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        Assignment a2 = new MathAssignment("Rolian Hansen", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+       Assignment a3 = new WritingAssignment("Evans Joel", "African History", "The Stop to the killing of Twins");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
