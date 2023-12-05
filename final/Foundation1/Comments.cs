7d4ebee62fff1d6f5e2a2fd6ab624373bd2fc031
evapearl <122605689+evapearl@users.noreply.github.com>
2023-12-05 15:40:50 +0100

    week12

---
+++ final/Foundation1/Comments.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Comment
+{
+    public string _name;
+    public string _commentTxt;
+
+    public void DisplayComment()
+    {
+        Console.WriteLine($">>{_name}: {_commentTxt}");
+    }
+}
